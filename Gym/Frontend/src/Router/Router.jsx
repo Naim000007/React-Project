8db6f70089fe6030e45e569c486ba2d7fba09732
Naim000007 <mdnaim01303202218@gmail.com>
2024-05-08 20:17:01 +0600

    project setup done

---
+++ Gym/Frontend/src/Router/Router.jsx
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello Naim Bhai!</div>,
+    },
+]);
+
+export default router
