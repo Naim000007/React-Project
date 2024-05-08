d41560bf8a2cf9c6208a5a1841b84312008c8ed7
Naim000007 <mdnaim01303202218@gmail.com>
2024-05-08 13:26:13 +0600

    section title add for diffreent sections

--- food/Client/src/pages/Home/Catagory/Catagory.jsx
+++ food/Client/src/pages/Home/Catagory/Catagory.jsx
@@ -9,10 +9,15 @@ import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
 import slide5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 
 export default function App() {
     return (
-        <>
+        <section>
+            <SectionTitle
+                subHeading={"From 11.00 AM to 10.00 PM"}
+                heading={"Order Online"}
+            />
             <Swiper
                 slidesPerView={4}
                 spaceBetween={30}
@@ -53,6 +58,6 @@ export default function App() {
                     </h3>
                 </SwiperSlide>
             </Swiper>
-        </>
+        </section>
     );
 }
