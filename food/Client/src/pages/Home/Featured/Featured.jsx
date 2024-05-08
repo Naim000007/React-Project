e8f83d6108cfdd49a49855a9a3fca407d76c803a
Naim000007 <mdnaim01303202218@gmail.com>
2024-05-08 16:49:34 +0600

    added a paralex effect

--- food/Client/src/pages/Home/Featured/Featured.jsx
+++ food/Client/src/pages/Home/Featured/Featured.jsx
@@ -5,15 +5,15 @@ import './Featured.css'
 
 const Featured = () => {
     return (
-        <div className='featured-item text-white'>
+        <div className='featured-item bg-fixed text-white'>
             <SectionTitle subHeading="Check it out" heading="Featured Items" />
-            <div className='md:flex justify-center items-center py-20 px-36'>
+            <div className='md:flex justify-center items-center bg-slate-500 bg-opacity-40 py-20 px-36'>
                 <div> <img src={FeaturedImg} /></div>
                 <div className='md:ml-10 '>
                     <p>May 5, 2024</p>
                     <p className='uppercase font-bold py-4'>where can i get some</p>
                     <p> Discover culinary bliss at BistroBoss, where every dish is a masterpiece. Indulge in our eclectic menu, showcasing the finest ingredients and innovative flavors. From savory starters to delectable desserts, each bite is a journey of delight. Join us for an unforgettable dining experience where passion meets perfection. </p>
-                    <button className="btn btn-outline mt-5">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-5">Order Now</button>
                 </div>
             </div>
         </div>
