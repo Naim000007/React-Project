8f143054b27dfe52255190dcaf58833f2cdac984
Naim000007 <mdnaim01303202218@gmail.com>
2024-05-08 12:29:54 +0600

    category section using swiper js

--- food/Client/src/pages/Home/Home.jsx
+++ food/Client/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './Banner/Banner'
+import Catagor from './Catagory/Catagory'
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Catagor />
         </div>
     )
 }
