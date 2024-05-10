82de0fe6be9234d27e58ca4459ad19801326d4c3
Naim000007 <mdnaim01303202218@gmail.com>
2024-05-10 12:13:29 +0600

    menu

--- food/Client/src/pages/Menu/Menu.jsx
+++ food/Client/src/pages/Menu/Menu.jsx
@@ -2,14 +2,36 @@ import React from 'react'
 import { Helmet } from 'react-helmet-async';
 import Cover from '../Shared/Cover';
 import menuImg from '../../assets/menu/banner3.jpg'
+import dessertImg from '../../assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../assets/menu/pizza-bg.jpg'
+import saladImg from '../../assets/menu/salad-bg.jpg'
+import soupImg from '../../assets/menu/soup-bg.jpg'
+import MenuCatagory from './MenuCatagory/MenuCatagory';
+import useMenu from '../../hooks/useMenu';
+import SectionTitle from '../../Components/SectionTitle/SectionTitle';
 
 const Menu = () => {
+    const [menu] = useMenu()
+    const desserts = menu.filter(item => item.category === "dessert")
+    const soup = menu.filter(item => item.category === "soup")
+    const pizza = menu.filter(item => item.category === "pizza")
+    const salad = menu.filter(item => item.category === "salad")
+    const offered = menu.filter(item => item.category === "offered")
     return (
         <div>
             <Helmet>
                 <title>Menu</title>
             </Helmet>
             <Cover img={menuImg} title='Our Menu' />
+            {/* main cover  */}
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer" />
+            {/* offered menu items  */}
+            <MenuCatagory items={offered} />
+            {/* dessert menu items */}
+            <MenuCatagory items={pizza} title={"pizza"} img={pizzaImg} />
+            <MenuCatagory items={desserts} title="Deessert" img={dessertImg} />
+            <MenuCatagory items={soup} title={"soup"} img={soupImg} />
+            <MenuCatagory items={salad} title={"salad"} img={saladImg} />
         </div>
     )
 }
--- food/Client/src/pages/Menu/MenuCatagory/MenuCatagory.jsx
+++ food/Client/src/pages/Menu/MenuCatagory/MenuCatagory.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import useMenu from '../../../hooks/useMenu'
+import { Helmet } from 'react-helmet-async'
+import Cover from '../../Shared/Cover'
+import MenuItem from '../../Shared/MenuItem'
 
-const MenuCatagory = () => {
+const MenuCatagory = ({ items, title, img }) => {
+
     return (
-        <div>
-
+        <div className='pt-8'>
+            {
+                title && <Cover img={img} title={title} />
+            }
+            <div className='grid md:grid-cols-2 gap-10 mt-16 '>
+                {
+                    items.map(item => <MenuItem key={item._id} item={item} />)
+                }
+            </div>
         </div>
     )
 }
