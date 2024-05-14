9c3a1dc1fc6b29f668db7e5ff3b9eb11327918bb
Naim000007 <mdnaim01303202218@gmail.com>
2024-05-14 09:58:29 +0600

    counter added

--- Agency/src/App.jsx
+++ Agency/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
 import Hero from './Components/UI/Hero'
+import Counter from './Components/UI/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       <Hero />
+      <Counter />
     </>
   )
 }
---
+++ Agency/src/Components/UI/Counter.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import "../../styles/Counter.css"
+
+const countrData = [
+    {
+        number: "3",
+        text: "Clients",
+    },
+    {
+        number: "07",
+        text: "Running Project",
+    },
+    {
+        number: "05",
+        text: "Project Completed",
+    }
+]
+
+const Counter = () => {
+    return (
+        <section className='counter'>
+            <div className='container'>
+                <div className='counter_wrapper'>
+                    {
+                        countrData.map((item, index) => (
+                            <div key={index} className="counter_item">
+                                <h3 className="couter_number">{item.number}</h3>
+                                <p className="counter_title">{item.text}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Counter
