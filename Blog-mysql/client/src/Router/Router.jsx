ac39faaabac931626550f5dc3dc2861d9c1de190
Naim000007 <mdnaim01303202218@gmail.com>
2024-05-16 09:25:29 +0600

    log in and register page added

---
+++ Blog-mysql/client/src/Router/Router.jsx
@@ -0,0 +1,61 @@
+import {
+    createBrowserRouter,
+    Outlet,
+} from "react-router-dom";
+import Register from "../page/Register";
+import Login from "../page/Login";
+import Single from "../page/Single";
+import Write from "../page/write";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import Home from "../page/Home";
+
+const Layout = () => {
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </div>
+    )
+}
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/post/:id",
+                element: <Single />,
+            },
+            {
+                path: "/write",
+                element: <Write />,
+            },
+        ]
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/single",
+        element: <Single />,
+    },
+    {
+        path: "/write",
+        element: <Write />,
+    },
+]);
+
+
+export default router;
+
--- Blog-mysql/client/src/page/Register.jsx
+++ Blog-mysql/client/src/page/Register.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Register = () => {
+const Login = () => {
     return (
-        <div>Register</div>
+        <div className='auth'>
+            <h1>Login</h1>
+            <form>
+                <input required type="text" placeholder="Username" />
+                <input required type="email" placeholder="email" />
+                <input required type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+                <p>This is an error</p>
+                <span> you have an account? <Link to="/login">Log in</Link></span>
+            </form>
+        </div>
     )
 }
 
-export default Register
+export default Login
