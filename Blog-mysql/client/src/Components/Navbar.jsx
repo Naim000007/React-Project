0b192fc7203a87d1deb4a0c6199b7f4611e7b72e
Naim000007 <mdnaim01303202218@gmail.com>
2024-05-16 10:11:42 +0600

    navbar added

--- Blog-mysql/client/src/Components/Navbar.jsx
+++ Blog-mysql/client/src/Components/Navbar.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css"
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
     return (
-        <div className='navbar'>
-            <div className="container"></div>
+        <div className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <Link className="logo" to="/">Naim</Link>
+                </div>
+                <div className="links">
+                    <Link className="link" to="/?cat=art">
+                        <h6>ART</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=science">
+                        <h6>SCIENCE</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=technology">
+                        <h6>TECHNOLOGY</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=cinema">
+                        <h6>CINEMA</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=design">
+                        <h6>DESIGN</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=food">
+                        <h6>FOOD</h6>
+                    </Link>
+                    <span className="right-span">Naim Sheikh</span>
+                    <span className="right-span">Log Out</span>
+                    <span className="write">
+                        <Link className="link" to="/write">Write</Link>
+                    </span>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
