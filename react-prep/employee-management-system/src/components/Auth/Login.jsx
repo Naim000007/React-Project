84584c2a0d1ec3141ee22d75056078022f318fa4
Naim000007 <mdnaim01303202218@gmail.com>
2024-11-02 17:56:59 +0600

    revise about usestate and from submit

--- react-prep/employee-management-system/src/components/Auth/Login.jsx
+++ react-prep/employee-management-system/src/components/Auth/Login.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     const submitHandler = (e) => {
         e.preventDefault()
-        console.log('Form submitted')
+        console.log('email is', email)
+        console.log('password is', password)
+        setEmail('')
+        setPassword('')
     }
     return (
         <>
@@ -12,8 +17,21 @@ const Login = () => {
                     <form
                         onSubmit={(e) => { submitHandler(e) }}
                         className='flex flex-col items-center justify-center'>
-                        <input required className='text-white outline-none bg-transparent border-2 border-emerald-600 text-xl py-4 px-5 rounded-full placeholder:text-gray-500 placeholder:text-xl' type="email" placeholder="Enter your Email" />
-                        <input required className='text-white outline-none bg-transparent border-2 border-emerald-600 mt-5 text-xl py-4 px-5 rounded-full placeholder:text-gray-500 placeholder:text-xl' type="password" placeholder="Enter your Email" />
+                        <input
+                            value={email}
+                            required className='text-white outline-none bg-transparent border-2 border-emerald-600 text-xl py-4 px-5 rounded-full placeholder:text-gray-500 placeholder:text-xl' type="email" placeholder="Enter your Email"
+                            onChange={(e) => {
+                                // console.log(e.target.value);
+                                setEmail(e.target.value);
+                            }} />
+                        <input
+                            required
+                            value={password}
+                            className='text-white outline-none bg-transparent border-2 border-emerald-600 mt-5 text-xl py-4 px-5 rounded-full placeholder:text-gray-500 placeholder:text-xl' type="password" placeholder="Enter your Email"
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                            }} />
+
                         <button className='text-white outline-none  border-none bg-emerald-600 text-xl mt-5 py-4 px-5 rounded-full placeholder:text-white placeholder:text-xl'>Log in</button>
                     </form>
                 </div>
@@ -23,3 +41,5 @@ const Login = () => {
 }
 
 export default Login
+
+
